3d187a8fa988d748c804776ae4d2e23d627977de
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2023-01-07 19:09:22 -0300

    feat: add ships list

---
+++ src/data/ships.js
@@ -0,0 +1,42 @@
+export const shipsInfos = [
+	{
+		id: 1,
+		baseCost: 50,
+		exponent: 1.15,
+		level: 0,
+		increment: 10,
+		shipImage: '/small-miner.svg'
+	},
+	{
+		id: 2,
+		baseCost: 1000,
+		exponent: 1.20,
+		level: 0,
+		increment: 50,
+		shipImage: '/medium-miner.svg'
+	},
+	{
+		id: 3,
+		baseCost: 10000,
+		exponent: 1.25,
+		level: 0,
+		increment: 250,
+		shipImage: '/heavy-miner.svg'
+	},
+	{
+		id: 4,
+		baseCost: 100000,
+		exponent: 1.3,
+		level: 0,
+		increment: 500,
+		shipImage: '/orbital-refinery.svg'
+	},
+	{
+		id: 5,
+		baseCost: 1000000,
+		exponent: 1.35,
+		level: 0,
+		increment: 1000,
+		shipImage: '/stelar-refinery.svg'
+	},
+]
